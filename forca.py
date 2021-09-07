6f3cc5b26929429fd6623cdd917b829554fbbf0d
Sergio Ramos <sergio.ramos@unemat.br>
2021-09-07 08:37:07 -0400

    estruturando jogo da forca

--- forca.py
+++ forca.py
@@ -4,6 +4,23 @@ def jogar():
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = True
+
+    while (not enforcou and not acertou):
+        chute = input("Escolha uma letra?")
+        chute = chute.strip()
+
+        index = 0
+
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição ".format(letra, index))
+            index+=1
+        print("jogando...")
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
