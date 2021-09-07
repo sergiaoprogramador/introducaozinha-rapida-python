af17d9e319d4e5b68cb2854131987240d3ded9a3
Sergio Ramos <sergio.ramos@unemat.br>
2021-09-06 23:15:34 -0400

    manipulando formatos

---
+++ format_brincando.py
@@ -0,0 +1,14 @@
+print("tentativa {} de {}".format(3,10))
+print("tentativa {1} de {0}".format(3,10))
+print("R$ {}".format(1.59))
+print("R$ {:f}".format(1.59))
+print("R$ {:.2f}".format(1.59))
+print("R$ {:.2f}".format(1.5))
+print("R$ {:7.2f}".format(1234.5))
+print("R$ {:7.2f}".format(4.5))
+print("R$ {:07.2f}".format(4.5))
+print("R$ {:07d}".format(4))
+print("Data {:02d}/{:02d}".format(9,4))
+
+nome = 'Sergio Ramos'
+print(f'Meu nome é {nome.lower()}')
--- take_guess.py
+++ take_guess.py
@@ -6,13 +6,19 @@ numero_secreto = 42
 total_tentativas = 3
 rodada = 1
 
-while(rodada <= total_tentativas):
+# range(start, stop, [step])
+for rodada in range(1, total_tentativas+1):
+# while(rodada <= total_tentativas):
     # String interpolation
     print("Tentativa {} de {}".format(rodada, total_tentativas))
 
-    chute_str = input("Digite o seu numero: ")
+    chute_str = input("Digite um numero entre 1 e 100: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um numero entre 1 e 100")
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
@@ -20,12 +26,11 @@ while(rodada <= total_tentativas):
 
     if(acertou):
         print("Você acertou!")
+        break
     else:
         if(maior):
             print("Você errou! Seu chute foi maior que o numero secreto")
         elif(menor):
             print("Você errou! Seu chute foi menor que o numero secreto")
-
-    rodada+= 1
 
 print("Fim do jogo")
